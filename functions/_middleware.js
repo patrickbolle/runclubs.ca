a115797ceff47f97f46647d9d4ed409de7c95afb
Patrick Bollenbach <patrick@bollenbach.ca>
2024-09-19 14:30:05 -0700

    New function file.

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,17 @@
-export async function onRequest({ request, next, env }) {
+
+
+  export async function onRequest({ request, env, next }) {
     const url = new URL(request.url);
-    
-    if (url.hostname === 'vancouver.runclubs.ca') {
-      url.pathname = '/vancouver' + url.pathname;
-      return env.ASSETS.fetch(url);
+    const subdomain = url.hostname.split('.')[0];
+  
+    if (subdomain !== 'www' && subdomain !== url.hostname) {
+      // Rewrite the URL to include the subdomain as a path
+      url.pathname = `/${subdomain}${url.pathname}`;
+      return next({
+        request: new Request(url.toString(), request)
+      });
     }
-    
+  
     return next();
   }
+  
