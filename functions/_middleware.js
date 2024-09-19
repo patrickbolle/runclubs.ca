b22f5a4b35ae0a2d046dc48a6d42e6b60b27ff9b
Patrick Bollenbach <patrick@bollenbach.ca>
2024-09-19 15:47:19 -0700

    More redirect troubleshooting

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,12 +2,13 @@
     const url = new URL(request.url);
     const subdomain = url.hostname.split('.')[0];
   
-    if (subdomain !== 'www' && subdomain !== url.hostname) {
-      // Rewrite the URL to include the subdomain as a path
-      url.pathname = `/${subdomain}${url.pathname}`;
-      return next({
-        request: new Request(url.toString(), request)
-      });
+    if (subdomain !== 'www' && subdomain !== 'runclubs' && subdomain !== url.hostname) {
+      // Check if the path is already set to the subdomain
+      if (!url.pathname.startsWith(`/${subdomain}`)) {
+        // Rewrite the URL to include the subdomain as a path
+        url.pathname = `/${subdomain}${url.pathname}`;
+        return Response.redirect(url.toString(), 301);
+      }
     }
   
     return next();
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			entries: ['*']
+			entries: ['*', '/vancouver', '/vancouver/*']
 		}
 	}
 };
